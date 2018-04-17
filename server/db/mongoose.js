d59e136952544e97f61a009257c441fd28f3b58c
Mike <yakovlev_miha@yahoo.com>
2018-04-17 14:16:18 +0300

    fin

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,9 +3,6 @@ const mongoose = require('mongoose');
 const uri = 'mongodb://mike:baba24Tunde@ds147469.mlab.com:47469/todos';
 
 mongoose.Promise = global.Promise;
-// mongoose.connect(uri);
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Users');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -29,31 +29,27 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
-  // res.send('hello world')
   })
 });
 
 
-// app.get('/todos/:id', (req, res) => {
-//   let id = req.params.id;
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
   
-//   if (!ObjectID.isValid(id)) {
-//     return res.status(404).send();
-//   }
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
   
-//   Todo.findById(id).then((todo) => {
-//     if (!todo) {
-//       return res.status(404).send();
-//     }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
 
-//     res.send({todo});
-//   }).catch((e) => {
-//     res.status(400).send();
-//   })
-// });
-
-// MongoDB URI
-// heroku config:set MONGOLAB_URI=mongodb://mike:baba24Tunde@ds111804.mlab.com:11804/test-todo
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
