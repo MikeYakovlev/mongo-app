a0e94d269ff3f55f88c02fe44126f6bbe25bdc6c
Mike <yakovlev_miha@yahoo.com>
2018-04-17 10:56:39 +0300

    344

--- server/server.js
+++ server/server.js
@@ -25,11 +25,12 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
-    res.send({todos});
-  }, (e) => {
-    res.status(400).send(e);
-  })
+  // Todo.find().then((todos) => {
+  //   res.send({todos});
+  // }, (e) => {
+  //   res.status(400).send(e);
+  res.send('hello world')
+  //})
 });
 
 
