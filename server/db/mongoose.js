9e72c4d6ea0dfba4ee73f7c654a236679d0ef69a
Mike <yakovlev_miha@yahoo.com>
2018-04-17 11:05:26 +0300

    6575

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,8 +4,9 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect(uri);
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Users');
+mongoose.connect('mongodb://mike:baba24Tunde@ds147469.mlab.com:47469/todos');
 
 module.exports = {mongoose};
 
